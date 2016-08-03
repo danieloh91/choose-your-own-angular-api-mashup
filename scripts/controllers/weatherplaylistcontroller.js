a3bccd69f5a06be20dc3515707891304b421d2fc
danieloh91 <daniel.oh.91@gmail.com>
2016-08-03 16:05:19 -0700

    search for weather and playlists display

--- scripts/controllers/weatherplaylistcontroller.js
+++ scripts/controllers/weatherplaylistcontroller.js
@@ -2,20 +2,18 @@ angular
   .module('weatherPlaylistApp')
   .controller('WeatherPlaylistController', WeatherPlaylistController);
 
-WeatherPlaylistController.$inject=['$http'];
-function WeatherPlaylistController($http) {
-  console.log("WeatherPlaylistController!");
-
+WeatherPlaylistController.$inject=['$http', '$routeParams'];
+function WeatherPlaylistController($http, $routeParams) {
   var vm = this;
 
-  $http({
-    method: 'GET',
-    url: 'https://api.spotify.com/v1/search?q=sunny&type=playlist'
-  }).then(function successCallback(succ) {
-    vm.playlists = succ.data.playlists.items;
-    console.log(vm.playlists);
-  }, function errorCallback(err) {
-    console.log('error', err);
-  });
-
+  vm.getPlaylists = function (query) {
+    $http({
+      method: 'GET',
+      url: 'https://api.spotify.com/v1/search?type=playlist&q=' + query
+    }).then(function successCallback(succ) {
+      vm.playlists = succ.data.playlists.items;
+    }, function errorCallback(err) {
+      console.log('error', err);
+    });
+  };
 }
